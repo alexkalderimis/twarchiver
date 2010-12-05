9aa827fa3181ebfd9a756c7d71c23cb81967feee
Alex Kalderimis <alex.kalderimis@gmail.com>
2010-12-05 18:22:43 +0000

    Refactored functions. Now single tweet and multiple tweet tagging actions use the same urls and callbacks.

--- public/javascripts/statuses.js
+++ public/javascripts/statuses.js
@@ -6,9 +6,27 @@ function addmasstags(user) {
     tagList = tags.join(",");
     var tweet_ids = tweetIds.join(",");
     createRequest();
-    var url = "/addmasstags/" + new Date().getTime();
+    var url = "/addtags/" + new Date().getTime();
     request.open("POST", url, true);
-    request.onreadystatechange = getMassTagAdder(tweetIds, tags, user);
+    request.onreadystatechange = getTagAdder(tweetIds, tags, user);
+    request.setRequestHeader("Content-Type",
+            "application/x-www-form-urlencoded");
+    request.send(
+            "&tags=" + escape(tagList) +
+            "&username=" + escape(user) +
+            "&tweetIds=" + escape(tweet_ids));
+}
+function deleteMassTags(user) {
+    var tweetIds = $.map($('form.tag-form'), function(el) {return el.id});
+    var tagFieldValue = document.getElementById('masstag').value;
+    var tags = tagFieldValue.split(",");
+    tags = jQuery.map(tags, function(str) {return trim(str)});
+    tagList = tags.join(",");
+    var tweet_ids = tweetIds.join(",");
+    createRequest();
+    var url = "/removetags/" + new Date().getTime();
+    request.open("POST", url, true);
+    request.onreadystatechange = getTagDeleter(tweetIds, tags, user);
     request.setRequestHeader("Content-Type",
             "application/x-www-form-urlencoded");
     request.send(
@@ -17,18 +35,125 @@ function addmasstags(user) {
             "&tweetIds=" + escape(tweet_ids));
 }
 
-function getMassTagAdder(tweetIds, tags, user) {
-    var tagListIds = jQuery.map(tweetIds, function(id) { return "tagList-" + id});
-    var tagLiNodes = jQuery.map(tags, function (tag) {
-        var liNode = document.createElement("li");
-        var tagText = document.createTextNode(tag);
-        liNode.appendChild(tagText);
-        return liNode;
-    });
-    return function() {};
+
+function getTagAdder(tweetIds, tags, user) {
+    return function() {
+        // if the request has responded
+        if (request.readyState == 4) {
+            var resultString = request.getResponseHeader("Results");
+            // if the request succeeded, 
+            // add the tags to the tweets & increment the counts
+            // or inform us if there are duplicates
+            if (request.status == 200) {
+                var messages = [];
+                var resultLists = resultString.split(",,");
+                for (i=0; i < tweetIds.length;i++) {
+                    var tagListId = "tagList-" + tweetIds[i];
+                    var tagListElem = document.getElementById(tagListId);
+                    var resultListString = resultLists[i];
+                    var resultList = resultListString.split(",");
+                    for (j=0; j < tags.length; j++) {
+                        var tag = tags[j];
+                        var result = resultList[j];
+                        if (result == "added") {
+                            addTagToTweet(tag, tagListElem, tag);
+                        } else {
+                            messages.push(result);
+                        }
+                    }
+                }
+                if (messages.length > 0) {
+                    alert(messages.join("\n"));
+                }
+            } else {
+                handleError(resultString, request);
+            }
+        }
+    };
 }
-    
+
+function addTagToTweet(tag, tagListElem, user) {
+    // first make the new li element
+    var liElem = document.createElement("li");
+    var tagText = document.createTextNode(tag);
+    liElem.appendChild(tagText);
+    // then add it onto the list
+    tagListElem.appendChild(liElem);
+    // Now either increment the count on the link, or add a new one
+    var tagLinks = jQuery('a.tagLink');
+    var foundLink = false;
+    // first try and find this tag in the list of links
+    for (k=0;k < tagLinks.length; k++) {
+        // match it against the link's tag
+        var tagK = tagLinks[k].getAttribute("tag");
+        if (tagK == tag) {
+            foundLink = true;
+            var link = tagLinks[k];
+            // increment the count
+            var tagCount = parseInt(link.getAttribute("count"));
+            tagCount++;
+            link.setAttribute("count", tagCount);
+            // change the link text
+            link.innerHTML = tag + " (" + tagCount + ")";
+        }
+    }
+    if (foundLink == false) {
+        // Than increment the global counter as well
+        var tagCounter = document.getElementById("tagCounter");
+        var count = parseInt(tagCounter.innerHTML);
+        tagCounter.innerHTML = count + 1;
+        // and add a new link tag list item to the tags list
+        var tagLinksList = document.getElementById('tagLinksList');
+        var listItem = document.createElement("li");
+        var link = document.createElement("a");
+        link.setAttribute("href",  "/show/" + user + "/tag/" + tag);
+        link.setAttribute("count", "1");
+        link.setAttribute("tag",   tag);
+        link.setAttribute("class", "tagLink");
+        var linkText = document.createTextNode(tag + " (1)");
+        listItem.appendChild(link);
+        link.appendChild(linkText);
+        tagLinksList.appendChild(listItem);
+    }
+}
+function removeTagFromTagList(tag, tagListElem) {
+    var tagLiElems = tagListElem.children;
+    for (j=0;j < tagLiElems.length; j++) {
+        text = tagLiElems[j].innerHTML;
+        if (text == tag) {
+            var tagLi = tagLiElems[j];
+            var parentNode = tagLi.parentNode;
+            $().add(tagLi).slideToggle('fast', function() {
+                tagLi.parentNode.removeChild(tagLi);
+            });
+            break;
+        }
+    }
+    var foundLink = false;
+    var tagLinks = jQuery('a.tagLink');
+    for (j=0;j < tagLinks.length; j++) {
+        var tagJ = tagLinks[j].getAttribute("tag");
+        if (tagJ == tag) {
+            foundLink = true;
+            var tagCount = parseInt(tagLinks[j].getAttribute("count"));
+            if (tagCount == 1) {
+                var tagLinksList = document.getElementById('tagLinksList');
+                tagLinksList.removeChild(tagLinks[j].parentNode);
+                count = parseInt(tagCounter.innerHTML);
+                tagCounter.innerHTML = Math.max(count - 1, 0);
+                break;
+            } else {
+                tagCount = tagCount - 1;
+                tagLinks[j].setAttribute("count", tagCount);
+                tagLinks[j].innerHTML = tag + " (" + tagCount + ")";
+                break;
+            }
+        }
+    }
+}
+
 function addNewTag(user, tweetId) {
+    var tweetIds = [tweetId];
     var tagFieldValue = document.getElementById("tag-" + tweetId).value;
     var tags = tagFieldValue.split(",");
     tags = jQuery.map(tags, function(str) {return trim(str)});
@@ -36,163 +161,82 @@ function addNewTag(user, tweetId) {
     createRequest();
     var url = "/addtags/" + new Date().getTime();
     request.open("POST", url, true);
-    request.onreadystatechange = getTagAdder(tweetId, tags, user);
+    request.onreadystatechange = getTagAdder(tweetIds, tags, user);
     request.setRequestHeader("Content-Type",
             "application/x-www-form-urlencoded");
     request.send(
             "&tags=" + escape(tagList) +
             "&username=" + escape(user) +
-            "&tweetId=" + escape(tweetId));
+            "&tweetIds=" + escape(tweetId));
     toggleDiv(tweetId);
 }
-function getTagAdder(tweetId, tags, user) {
-    var tagListId = "tagList-" + tweetId;
-    var tagLiNodes = jQuery.map(tags, function (tag) {
-        var liNode = document.createElement("li");
-        var tagText = document.createTextNode(tag);
-        liNode.appendChild(tagText);
-        return liNode;
-    });
-    return function() {
-        if (request.readyState == 4) {
-            var resultList = request.getResponseHeader("Status");
-            if (request.status == 200) {
-                var messages = [];
-                var tagList = document.getElementById(tagListId);
-                var tagCounter = document.getElementById("tagCounter");
-                var results = resultList.split(",");
-                for (i=0; i < tags.length; i++) {
-                    var tag = tags[i];
-                    var result = results[i];
-                    if (result == "added") {
-                        tagList.appendChild(tagLiNodes[i]);
-                        var tagLinks = jQuery('a.tagLink');
-                        var foundLink = false;
-                        for (j=0;j < tagLinks.length; j++) {
-                            var linkText = tagLinks[j].innerHTML;
-                            if (starts_with(linkText, tag)) {
-                                foundLink = true;
-                                var tagCount = parseInt(tagLinks[j].getAttribute("count"));
-                                tagCount++;
-                                tagLinks[j].setAttribute("count", tagCount);
-                                tagLinks[j].innerHTML = tag + " (" + tagCount + ")";
-                            }
-                        }
-                        if (foundLink == false) {
-                            var count = parseInt(tagCounter.innerHTML);
-                            tagCounter.innerHTML = count + 1;
-                            var tagLinksList = document.getElementById('tagLinksList');
-                            var liElement = document.createElement("li");
-                            var aElement = document.createElement("a");
-                            aElement.setAttribute("href", "/show/" + user + "/tag/" + tag);
-                            aElement.setAttribute("count", "1");
-                            aElement.setAttribute("class", "tagLink");
-                            var aText = document.createTextNode(tag + " (1)");
-                            liElement.appendChild(aElement);
-                            aElement.appendChild(aText);
-                            tagLinksList.appendChild(liElement);
-                        }
-                    } else {
-                        messages.push(result);
-                    }
-                } 
-                if (messages.length > 0) {
-                    alert(messages.join("\n"));
-                }
-            } else {
-                if ((resultList == null) || (resultList.length == null) || (resultList.length <= 0)) {
-                    alert("Sorry - adding tags failed. " + request.status);
-                } else {
-                    alert(resultList);
-                }
-            }
-        }
-    };
-} 
-
 
 function deleteTag(user, tweetId) {
+    var tweetIds = [tweetId];
     var tagFieldValue = document.getElementById("tag-" + tweetId).value;
     var tags = tagFieldValue.split(",");
     tags = jQuery.map(tags, function(str) {return trim(str)});
     tagList = tags.join(",");
     createRequest();
-    var url = "/deletetags/" + new Date().getTime();
+    var url = "/removetags/" + new Date().getTime();
     request.open("POST", url, true);
-    request.onreadystatechange = getTagDeleter(tweetId, tags, user);
+    request.onreadystatechange = getTagDeleter(tweetIds, tags, user);
     request.setRequestHeader("Content-Type",
             "application/x-www-form-urlencoded");
     request.send(
             "&tags=" + escape(tagList) +
             "&username=" + escape(user) +
-            "&tweetId=" + escape(tweetId));
+            "&tweetIds=" + escape(tweetId));
     toggleDiv(tweetId);
 }
 
-function getTagDeleter(tweetId, tags, user) {
-    var tagListId = "tagList-" + tweetId;
+function getTagDeleter(tweetIds, tags, user) {
     return function() {
         if (request.readyState == 4) {
-            var resultList = request.getResponseHeader("Status");
+            var resultString = request.getResponseHeader("Results");
             if (request.status == 200) {
                 var messages = [];
-                var tagList = document.getElementById(tagListId);
+                var resultLists = resultString.split(",,");
                 var tagCounter = document.getElementById("tagCounter");
-                var results = resultList.split(",");
+                for (var i=0; i < tweetIds.length; i++) {
+                    var resultListString = resultLists[i];
+                    var resultList = resultListString.split(",");
 
-                for (i=0; i < tags.length; i++) {
-                    var tag = tags[i];
-                    var result = results[i];
-                    if (result == "deleted") {
-                        var tagLiElems = tagList.children;
-                        for (j=0;j < tagLiElems.length; j++) {
-                            text = tagLiElems[j].innerHTML;
-                            if (text == tag) {
-                                var tagLi = tagLiElems[j];
-                                var parentNode = tagLi.parentNode;
-                                $().add(tagLi).slideToggle('fast', function() {
-                                    //tagLi.parentNode.removeChild(tagLi);
-                                });
-                                break;
-                            }
+                    var tagListId = "tagList-" + tweetIds[i];
+                    var tagListElem = document.getElementById(tagListId);
+
+                    for (var j=0; j < tags.length; j++) {
+                        var tag = tags[j];
+                        var result = resultList[j];
+                        if (result == "deleted") {
+                            removeTagFromTagList(tag, tagListElem);
+                        } else {
+                            messages.push(result);
                         }
-                        var tagLinks = jQuery('a.tagLink');
-                        for (j=0;j < tagLinks.length; j++) {
-                            var linkText = tagLinks[j].innerHTML;
-                            if (starts_with(linkText, tag)) {
-                                foundLink = true;
-                                var tagCount = parseInt(tagLinks[j].getAttribute("count"));
-                                if (tagCount == 1) {
-                                    var tagLinksList = document.getElementById('tagLinksList');
-                                    tagLinksList.removeChild(tagLinks[j].parentNode);
-                                    count = parseInt(tagCounter.innerHTML);
-                                    tagCounter.innerHTML = Math.max(count - 1, 0);
-                                    break;
-                                } else {
-                                    tagCount = tagCount - 1;
-                                    tagLinks[j].setAttribute("count", tagCount);
-                                    tagLinks[j].innerHTML = tag + " (" + tagCount + ")";
-                                    break;
-                                }
-                            }
-                        }
-                    } else {
-                        messages.push(result);
                     }
                 } 
                 if (messages.length > 0) {
                     alert(messages.join("\n"));
                 }
             } else {
-                if ((resultList == null) || (result.length == null) || (result.length <= 0)) {
-                    alert("Sorry - deleting tag failed. " + request.status);
-                } else {
-                    alert(resultList);
-                }
+                handleError(resultString, request);
             }
         }
     };
 } 
+
+function handleError(resultString, request) {
+    var message;
+    if ((resultString == null) 
+            || (resultString.length == null) 
+            || (resultString.length <= 0)) {
+        message = "Sorry - request failed.";
+    } else {
+        message = resultString;
+    }
+    alert(message + request.status);
+}
+
 
 function toggleDiv(divid){
     $("#" + divid).slideToggle('fast', function() {
