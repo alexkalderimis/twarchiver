89f6b2ab6560a43dc58bec7ce80ea3f87757b2aa
Alex Kalderimis <alex.kalderimis@gmail.com>
2010-12-18 22:18:19 +0000

    Updated javascripts for new style page - and added individual deleters.

--- public/javascripts/statuses.js
+++ public/javascripts/statuses.js
@@ -11,7 +11,7 @@ function addTags(user, tweetId) {
     var tagFieldValue = document.getElementById("tag-" + tweetId).value;
     var tags = tagFieldValue.split(",");
     tags = jQuery.map(tags, function(str) {return trim(str)});
-    toggleDiv(tweetId);
+    ensureHidden(tweetId);
     requestAddTags(user, tags, tweetIds);
 }
 function requestAddTags(user, tags, tweetIds) {
@@ -47,7 +47,14 @@ function removeTags(user, tweetId) {
     var tags = tagFieldValue.split(",");
     tags = jQuery.map(tags, function(str) {return trim(str)});
     requestRemoveTags(user, tags, tweetIds);
-    toggleDiv(tweetId);
+    ensureHidden(tweetId);
+}
+
+function removeTag(user, tweetId, tag) {
+    var tweetIds = [tweetId];
+    var tags = [tag];
+    requestRemoveTags(user, tags, tweetIds);
+    ensureHidden(tweetId);
 }
 
 function requestRemoveTags(user, tags, tweetIds) {
@@ -78,26 +85,48 @@ function getTagAdder(user) {
                 messages.push(value);
             } else {
                 var tweetId = key;
+                console.log("Tweet id is " + tweetId);
                 var addedTags = value.added;
-                var tagListId = "tagList-" + tweetIds[i];
+                var tagListId = "tagList-" + tweetId;
+                console.log("looking for a taglist called " + tagListId);
                 var tagListElem = document.getElementById(tagListId);
+                console.log(tagListElem);
                 for (var i = 0; i < addedTags.length; i++) {
-                    addTagToTweet(addedTags[i], tagListElem);
+                    addTagToTweet(addedTags[i], tagListElem, tweetId, user);
                 }
             }
-        };
+        });
         if (messages.length > 0) {
             alert(messages.join("\n"));
         }
-        updateSideBars(user);
+        updateTags(user);
     };
 }
 
-function addTagToTweet(tag, tagListElem, user) {
+function addTagToTweet(tag, tagListElem, tweetId, user) {
     // first make the new li element
     var liElem = document.createElement("li");
+
+    var spanElem = document.createElement("span");
     var tagText = document.createTextNode(tag);
-    liElem.appendChild(tagText);
+    spanElem.setAttribute("onclick", 
+            "toggleElem('" + tweetId + '-' + tag + "')");
+    spanElem.appendChild(tagText);
+
+    var spacer = document.createTextNode("   ");
+
+    var aElem = document.createElement("a");
+    aElem.setAttribute("style", "display: none");
+    aElem.setAttribute("href", '#');
+    aElem.setAttribute("id", tweetId + '-' + tag);
+    aElem.setAttribute("onclick", 
+        "removeTag('" + user  + "', '" + tweetId + "', '" + tag + "')");
+    var aText = document.createTextNode("delete");
+    aElem.appendChild(aText);
+
+    liElem.appendChild(spanElem);
+    liElem.appendChild(spacer);
+    liElem.appendChild(aElem);
     // then add it onto the list
     tagListElem.appendChild(liElem);
 }
@@ -105,8 +134,8 @@ function addTagToTweet(tag, tagListElem, user) {
 function removeTagFromTagList(tag, tagListElem) {
     var tagLiElems = tagListElem.children;
     for (j=0;j < tagLiElems.length; j++) {
-        text = tagLiElems[j].innerHTML;
-        if (text == tag) {
+        var firstKid = tagLiElems[j].children[0];
+        if (firstKid.innerHTML == tag) {
             var tagLi = tagLiElems[j];
             var parentNode = tagLi.parentNode;
             $().add(tagLi).slideToggle('fast', function() {
@@ -117,6 +146,21 @@ function removeTagFromTagList(tag, tagListElem) {
     }
 }
 
+function toggleElem(obj) {
+    var el = document.getElementById(obj);
+    if ( el.style.display != 'none' ) {
+        el.style.display = 'none';
+    }
+    else {
+        el.style.display = '';
+    }
+}
+
+function ensureHidden(obj) {
+    var el = document.getElementById(obj);
+    el.style.display = 'none';
+}
+
 function getTagDeleter(user) {
     return function(data) {
         var messages = [];
@@ -125,25 +169,24 @@ function getTagDeleter(user) {
                 messages.push(value);
             } else {
                 var tweetId = key;
-                var addedTags = value.added;
-                var tagListId = "tagList-" + tweetIds[i];
+                var removedTags = value.removed;
+                var tagListId = "tagList-" + tweetId;
                 var tagListElem = document.getElementById(tagListId);
-                for (var i = 0; i < addedTags.length; i++) {
-                    removeTagFromTagList(addedTags[i], tagListElem);
+                for (var i = 0; i < removedTags.length; i++) {
+                    removeTagFromTagList(removedTags[i], tagListElem);
                 }
             }
-        };
+        });
         if (messages.length > 0) {
             alert(messages.join("\n"));
         }
-        updateSideBars(user);
+        updateTags(user);
     };
 }
 
 function handleError(request, resultString) {
     alert(resultString);
 }
-
 
 function toggleDiv(divid){
     $("#" + divid).slideToggle('fast', function() {
@@ -156,14 +199,31 @@ function toggleForm(divId) {
     });
 }
 
+function loadSummary(data) {
+    console.log("loading summary data");
+    console.log("data", data);
+    document.getElementById('beginning').innerHTML = data.beginning;
+    document.getElementById('most_recent').innerHTML = data.most_recent;
+    document.getElementById('tweet_count').innerHTML = data.tweet_count;
+    document.getElementById('mention_count').innerHTML = data.mention_count;
+    document.getElementById('hashtag_count').innerHTML = data.hashtag_count;
+    document.getElementById('tag_count').innerHTML = data.tag_count;
+    document.getElementById('retweet_count').innerHTML = data.retweet_count;
+}
+
+function updateTags(user) {
+    $("#tagLinksList").load("/load/tags/for/" + user);
+    $.get("/load/summary/" + user, null, 
+        loadSummary, "json");
+}
+
 function updateSideBars(user) {
     $("#timeline-list").load("/load/timeline/for/" + user);
     $("#mentions-list").load("/load/mentions/for/" + user);
     $("#hashtags-list").load("/load/hashtags/for/" + user);
     $("#urls-list").load("/load/urls/for/" + user);
-    $("#tagLinksList").load("/load/tags/for/" + user);
     $("#retweetedLinksList").load("/load/retweeteds/for/" + user);
-    $("#favedLinksList").load("/load/favourites/for/" + user);
+    updateTags(user);
 }
 /**
 *
